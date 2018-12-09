0735b5c436dbd3541e40a362def671daafeb179c
MatyasKonig <mchlebovsky@icloud.com>
2018-12-09 17:19:06 -0500

    Full selection

--- Analysis.R
+++ Analysis.R
@@ -1,10 +1,11 @@
 library(leaps)
 
 data = read.csv('cleaned_data.csv', sep = ",", na.strings = '?')
+data$state = as.factor(data$state)
 
 summary(data)
-str(data)
+str(data$state)
 
 # forward selection
-regfit.fwd = regsubsets(ViolentCrimesPerPop ~ . - communityname, data  = data, nvmax = 10, method = "forward")
+regfit.fwd = regsubsets(ViolentCrimesPerPop ~ . - communityname - state - fold - state_abv, data  = data, nvmax = 10, method = "forward")
 summary(regfit.fwd)
