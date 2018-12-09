0a2fac8ec72f0fb801c42756dd7368110d222a21
dnphoa <33738662+dnphoa@users.noreply.github.com>
2018-12-09 14:21:37 -0500

    stuff
    remove nan columns
    
    add column names
    
    concat state abv
    
    choosing threshold for dangerous neighborhood

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -9,10 +9,29 @@ Created on Sat Dec  1 23:10:37 2018
 import re
 import pandas as pd
 
-with open('variable names.txt', 'r') as inFile:
+
+with open('variable names.txt', 'r') as inFile: 
+    #var name is txt file with variable names
 	varNames = inFile.read()
 	varComp = re.compile(r'--\s(\w+)')
+    #use regular expression to extract var names from string
 	varMatch = varComp.findall(varNames)
-	df = pd.read_csv('communities.csv',names = varMatch)
+	df = pd.read_csv('communities.csv',names = varMatch,na_values=['?'])
 
 
+#remove columns with nans
+df = df.dropna(axis='columns')
+
+#import state code file
+dfState = pd.read_csv('state.txt',sep='|',header=0)
+#remove two unwanted columns
+dfState = dfState.drop(['STATE_NAME','STATENS'],axis=1)
+dfState.columns = ['state','state_abv']
+
+df = pd.merge(df,dfState[['state','state_abv']],on='state',how='left')
+
+#define threshold for dangerous cities that are in the .95 quantile 
+dangerThreshold = df['ViolentCrimesPerPop'].quantile(.90)
+
+
+df[df['ViolentCrimesPerPop'] >= dangerThreshold]
